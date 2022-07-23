8656910db67f6ab8c70148492780c7a15d22862d
Cherepanov Kirill <cherkirr@gmail.com>
2022-07-23 10:10:53 +0300

    init: start a react+ts+tailwind e-commerse project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() { 
+  return (
+    <div className="text-green-500">Hi</div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ['./src/**/*.{ts,tsx}', './public/index.html'],
+  theme: {},
+  plugins: []
+};
