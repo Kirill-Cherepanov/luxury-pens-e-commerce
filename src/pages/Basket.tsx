53213895a97ac4e6c95e239bc9ba969d7a5908af
Cherepanov Kirill <cherkirr@gmail.com>
2022-07-24 19:48:06 +0300

    feat: add basic layout and styling to Basket page

--- src/pages/Basket.tsx
+++ src/pages/Basket.tsx
@@ -27,65 +27,77 @@ export default function Basket() {
   if (cartItems.length === 0) return null;
 
   return (
-    <div className="px-6 pt-4">
-      <div className="flex align-center border-b border-b-slate-400 pb-2 mb-2">
-        <NavLink to="/Store" className="flex flex-col justify-end basis-1/3">
-          Continue shopping
-        </NavLink>
-        <h1 className="text-center font-bold text-xl basis-1/3">BASKET</h1>
+    <div className="pt-4">
+      <div className="px-6">
+        <div className="flex align-center border-b-2 border-b-slate-400 pb-4">
+          <NavLink to="/Store" className="flex flex-col justify-end basis-1/3">
+            Continue shopping
+          </NavLink>
+          <h1 className="text-center font-bold text-xl basis-1/3">BASKET</h1>
+        </div>
       </div>
-      <ul>
+      <ul className="px-6 mb-10">
         {cartItems.map((item) => (
-          <BasketItem key={item.id} {...item} />
+          <BasketItem
+            className=" my-4 border-b border-b-slate-600"
+            key={item.id}
+            {...item}
+          />
         ))}
       </ul>
       <div>
-        <div>
-          <p>
+        <div className="px-6 mb-10">
+          <p className="text-red-500 text-justify">
             Please, check carefully your order and your contact information
             before clicking on the "Order" button. No alteration will be
             accepted afterward.
           </p>
         </div>
-        <div className="ORDER-SUMMARY">
-          <h3>Order summary</h3>
-          <div>
-            <span>Total item cost:</span>
-            <span>{formatCurrency(total)}</span>
-          </div>
-          <div>
-            <span>Shipping charge:</span>
-            <span>{formatCurrency(0)}</span>
-          </div>
-          <div>
-            <div>
-              <span>Total amount:</span>
-              <span>{formatCurrency(total)}</span>
+        <div className="ORDER-SUMMARY bg-slate-200 mb-8">
+          <div className="px-6 py-8">
+            <h3 className="text-lg font-bold mb-4">Order summary</h3>
+            <div className="flex justify-between">
+              <span className="text-sm">Total item cost:</span>
+              <span className="font-bold">{formatCurrency(total)}</span>
             </div>
-            <button>Confirm my order</button>
+            <div className="flex justify-between">
+              <span className="text-sm">Shipping charge:</span>
+              <span className="font-bold">{formatCurrency(0)}</span>
+            </div>
+          </div>
+          <div className="px-6 py-5 bg-slate-600 text-white">
+            <div className="flex justify-between mb-4">
+              <span className="">TOTAL AMOUNT:</span>
+              <span className="font-bold">{formatCurrency(total)}</span>
+            </div>
+            <button className="block mx-auto bg-white text-slate-600 rounded-full px-4 py-2">
+              Confirm my order
+            </button>
           </div>
         </div>
       </div>
-      <div>
-        <div>
-          <h4>DELIVERY</h4>
-          <p>Free delivery across the globe. Possible return under 14 days.</p>
+      <div className="px-4 pb-8">
+        <div className="mb-8 w-3/4 mx-auto text-center">
+          <h4 className="mb-1 font-bold text">DELIVERY</h4>
+          <p className="text-sm">
+            Free delivery across the globe. Possible return under 14 days.
+          </p>
         </div>
-        <div>
-          <h4>SECURE PAYMENT</h4>
-          <p>
+        <div className="mb-8 w-3/4 mx-auto text-center">
+          <h4 className="mb-1 font-bold text">SECURE PAYMENT</h4>
+          <p className="text-sm">
             Visa, Mastercard, Post Finance, Paypal, Twint, WebMoney, CIPS,
             EasyPay, etc.
           </p>
         </div>
-        <div>
-          <h4>CUSTOMER SERVICE</h4>
-          <p>
+        <div className=" w-3/4 mx-auto text-center">
+          <h4 className="mb-1 font-bold text">CUSTOMER SERVICE</h4>
+          <p className="text-sm">
             Monday to Friday from 10am to 7pm and Saturday from 10am to 5pm.
           </p>
-          <div>+1 (213) 123-4567</div>
+          <div className="my-2">+1 (213) 123-4567</div>
           <p>
-            <small>
+            <small className="italic">
               (Calls from USA : at local rate. Calls from abroad : at telecom
               provider's international rate.)
             </small>
@@ -98,16 +110,17 @@ export default function Basket() {
 
 type BaksetItemProps = {
   id: number;
+  className: string;
 };
 
-function BasketItem({ id }: BaksetItemProps) {
+function BasketItem({ id, className }: BaksetItemProps) {
   const [isPreviewOpen, setIsPreviewOpen] = useState(false);
   const { removeItem } = useShoppingCart();
   const item = storeItems.find((item) => item.id === id);
   if (item === undefined) return null;
 
   return (
-    <div className="flex flex-col border-b border-b-slate-800 mb-2">
+    <div className={'flex flex-col ' + className}>
       <button onClick={() => removeItem(id)} className="ml-auto mb-4">
         <img src={closeIcon} width="16" height="16" alt="" />
       </button>
